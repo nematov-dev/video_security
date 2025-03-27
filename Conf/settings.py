85a4242e4982ab1497b901c3cb79ec9e1447e2a0
Ne'matov Saidakbar <Saidakbarnematov2509@gamil.com>
2025-03-27 21:26:52 +0500

    add assets folder

--- Conf/settings.py
+++ Conf/settings.py
@@ -108,8 +108,8 @@ AUTH_USER_MODEL = 'video_security_app.Worker'
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles/'
-STATICFILES_DIRS = [BASE_DIR / 'static/']
+STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'assets/']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
